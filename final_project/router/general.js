e6ee476a81592b082109d91055143af0a72457ec
m-kuber <kishore.kuber23@vit.edu>
2026-04-15 06:48:50 -0400

    Add functionality to get books by author name

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,9 +22,17 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let result = {};
+
+    Object.keys(books).forEach((key) => {
+        if (books[key].author === author) {
+            result[key] = books[key];
+        }
+    });
+
+    return res.send(JSON.stringify(result, null, 4));
 });
 
 // Get all books based on title
